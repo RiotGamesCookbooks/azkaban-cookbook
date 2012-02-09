d8d4c678113f118ecf4fcdb70e27e8bedad857d2
Jamie Winsor <jamie@vialstudios.com>
2012-02-09 15:15:30 -0800

    add os dependency lock to metadata

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,7 @@ license          "Apache 2.0"
 description      "Installs and configures azkaban"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+%w{ centos redhat fedora }.each do |os|
+  supports os
+end
